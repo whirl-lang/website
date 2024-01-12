1ec7fb58ba1cb0ae4571eea8e27342629dac67a0
Robert Zuchniak <robert.zuchniak@hotmail.com>
2024-01-12 14:27:52 -0500

    feat: add install page

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -5,18 +5,18 @@
         <h2 class="text-5xl pt-16 pb-8 pl-8">
             Why Whirl?
         </h2>
-        <div class="flex gap-16 justify-center">
-            <div class="flex-1 w-1/4">
+        <div class="flex gap-16 justify-center flex-wrap">
+            <div class="flex-1 min-w-96">
             <p class="p-8">
                 Empower your programming journey with Whirl, the language of innovation and efficiency. Whether you're a seasoned developer or just starting your coding adventure, our platform is designed to cater to all skill levels. Dive into the world of Whirl and unlock the potential to create robust, scalable, and cutting-edge applications.
             </p>
         </div>
-        <div class="flex-1 min-w-150">
+        <div class="flex-1 min-w-96">
             <p class="p-8">
                 Whirl is a new programming language that is designed to be easy to learn and use. It's also designed to be fast, secure, and scalable. Whirl is a statically typed language with a strong type system. It's also a functional language with a powerful type inference system. Whirl is a general-purpose programming language that is designed to be used for a wide variety of applications. It's also a general-purpose programming language that is designed to be used for a wide variety of applications. Whirl is a general-purpose programming language that is designed to be used for a wide variety of applications. It's also a general-purpose programming language that is designed to be used for a wide variety of applications. Whirl is a general-purpose programming language that is designed to be used for a wide variety of applications. It's also a general-purpose programming language that is designed to be used for a wide variety of applications.
             </p>
         </div>
-        <div class="flex-1 min-w-150">
+        <div class="flex-1 min-w-96">
             <p class="p-8">
                 Whirl is a general-purpose programming language that is designed to be used for a wide variety of applications. It's also a general-purpose programming language that is designed to be used for a wide variety of applications. Whirl is a general-purpose programming language that is designed to be used for a wide variety of applications. It's also a general-purpose programming language that is designed to be used for a wide variety of applications. Whirl is a general-purpose programming language that is designed to be used for a wide variety of applications. It's also a general-purpose programming language that is designed to be used for a wide variety of applications.
             </p>
--- src/routes/install/+page.svelte
+++ src/routes/install/+page.svelte
@@ -1,12 +1,22 @@
 <div class="p-16">
   <div>
-    <h1 class="text-5xl">Install</h1>
-    <div class="mockup-code pt-16 w-40 bg-black">
-        <div class="mockup-code">
-            <pre data-prefix="$"><code>git clone https://github.com/whirl-lang/whirl</code></pre> 
-            <pre data-prefix="$"><code>cd whirl</code></pre> 
-            <pre data-prefix="$"><code>go build cmd/whirl/main.go!</code></pre>
-          </div>
+    <h1 class="text-5xl pb-8">Install</h1>
+    <div class="mockup-code bg-black">
+      <pre data-prefix="$"><code
+          >git clone https://github.com/whirl-lang/whirl</code
+        ></pre>
+      <pre data-prefix="$"><code>cd whirl</code></pre>
+      <pre data-prefix="$"><code>go build cmd/whirl/main.go!</code></pre>
+    </div>
+  </div>
+  <div>
+    <h1 class="text-5xl pb-8 pt-16 ">Usage</h1>
+    <div class="mockup-code bg-black">
+      <pre data-prefix="$"><code
+          >git clone https://github.com/whirl-lang/whirl</code
+        ></pre>
+      <pre data-prefix="$"><code>cd whirl</code></pre>
+      <pre data-prefix="$"><code>go build cmd/whirl/main.go!</code></pre>
     </div>
   </div>
 </div>
