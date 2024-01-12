4fc739bbff9fee9c7e0478771a314c87cc79da18
Robert Zuchniak <robert.zuchniak@hotmail.com>
2024-01-12 14:41:44 -0500

    feat: add text to install page

--- src/routes/install/+page.svelte
+++ src/routes/install/+page.svelte
@@ -10,13 +10,20 @@
     </div>
   </div>
   <div>
-    <h1 class="text-5xl pb-8 pt-16 ">Usage</h1>
+    <h1 class="text-5xl pb-8 pt-16">Usage</h1>
     <div class="mockup-code bg-black">
-      <pre data-prefix="$"><code
-          >git clone https://github.com/whirl-lang/whirl</code
+      <pre><code
+          >Usage:</code
         ></pre>
-      <pre data-prefix="$"><code>cd whirl</code></pre>
-      <pre data-prefix="$"><code>go build cmd/whirl/main.go!</code></pre>
+      <pre data-prefix="$"><code
+          >whirl &lt;PATH&gt;</code
+        ></pre>
+      <pre><code>Arguements:</code></pre>
+      <pre data-prefix="$"><code>&lt;PATH&gt;   Path to the script to execute</code></pre>
+      <pre ><code></code></pre>
+      <pre ><code>Options:</code></pre>
+      <pre data-prefix="$"><code>-h, --help      Print help</code></pre>
+      <pre data-prefix="$"><code>-v, --version   Pring version</code></pre>
     </div>
   </div>
 </div>
